35398bd8879e9f41acaf69995f5df2ace9ccbbfd
Aaron Selvig <aaronselvig@gmail.com>
2010-11-12 17:42:19 -0800

    Initial commit

---
+++ Gemfile
@@ -0,0 +1,5 @@
+source 'http://rubygems.org'
+
+gem 'rails', '3.0.1'
+gem 'sqlite3-ruby', '1.3.1', :group => :development, :require => 'sqlite3' #":group => :development" makes it work on heroku
+gem "mongrel", ">= 1.2.0.pre2"
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = 'd1f5fd805e42c8d02477d4f1cae8335b07ef7035b37fa8cc581ecfb70b511ef2a6dc03dc293663de1a47c0cb5edf2d1d161338983dbe62a3ddbdce114d23ff26'
